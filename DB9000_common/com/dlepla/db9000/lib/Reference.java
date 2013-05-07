457b8ab611f5895a5e22870459fc4711d55cdfb5
Drew LePla <dblepla@gmail.com>
2013-05-07 00:05:39 -0400

    Created several more methods to handle writing/reading objects directly to/from a binary file. Got the password file working. need to re-implement encryption.

--- DB9000_common/com/dlepla/db9000/lib/Reference.java
+++ DB9000_common/com/dlepla/db9000/lib/Reference.java
@@ -14,9 +14,11 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import javax.swing.Box;
@@ -31,28 +33,30 @@ import com.dlepla.db9000.User;
 
 public class Reference
 {
-    
-    
+
     // Initializes and sets static color objects for the DB9000 program.
-    
+
     public static final Color HEADER_TEXT_COLOR = new Color(232, 255, 210);
     public static final Color HEADER_BACKGROUD_COLOR = new Color(34, 177, 76);
     public static final Color HEADER_BORDER_COLOR = new Color(17, 89, 38);
     public static final Color CENTER_BACKGROUND_COLOR = new Color(226, 255, 198);
     public static final Color FOOTER_BACKGROUND_COLOR = new Color(236, 255, 217);
-    
-    public static final char[] DEFAULT_PASSWORD = new char[] { 'A', 'q', '2', '3', 'e', 's',
-                'a', 'Q', '@', '#', 'E', 'S' };
+
+    public static final char[] DEFAULT_PASSWORD = new char[] { 'A', 'q', '2',
+            '3', 'e', 's', 'a', 'Q', '@', '#', 'E', 'S' };
     public static final String DEFAULT_USERNAME = "dblepla";
-    
-    // Initializes and sets file paths for image files used by the DB9000 program
-    
+
+    // Initializes and sets file paths for image files used by the DB9000
+    // program
+
     public static final String X16_ICON_LOCATION = "C:\\Development\\source\\DB9000\\resources\\Icon16x16.gif";
-    public static final Path PASSWORD_FILE = Paths.get("C:\\Development\\source\\DB9000\\resources\\bin\\dbaccess.dat");
-    public static final Path DBDB_FILE = Paths.get("C:\\Development\\source\\DB9000\\resources\\bin\\DBDB.dat");
-    
-    
-    
+    public static final Path PASSWORD_FILE = Paths
+            .get("C:\\Development\\source\\DB9000\\resources\\bin\\dbaccess.dat");
+    public static final Path DBDB_FILE = Paths
+            .get("C:\\Development\\source\\DB9000\\resources\\bin\\DBDB.dat");
+    public static final Path KEY_FILE = Paths
+            .get("C:\\Development\\source\\DB9000\\resources\\bin\\keyfile");
+
     // Creating a helper method that allows us to easily add JComponents to a
     // JPanel using GridBagConstraints
     public static void addItem(JPanel p, JComponent c, int x, int y, int width,
@@ -76,280 +80,332 @@ public class Reference
         // GridBagConstraints set in gc
         p.add(c, gc);
     }
-    
-    
+
     // Defines a helper method for Authorizing usernames and passwords
-    
+
     public static boolean authLogin(User testUser)
     {
-        
-        User[] authorizedUsers;
-        
-        DataInputStream passFile = getStream("dbaccess.dat");
-        
+
+        User[] authorizedUsers = readUser(PASSWORD_FILE.toString());
+
         System.out.print("Entered Password: ");
         for (char c : testUser.password)
         {
             System.out.print(c);
         }
-        
-        
+
+        for (User u : authorizedUsers)
+        {
+            System.out.print("Users from File: " + u.toString());
+        }
+
         System.out.print("\n");
         System.out.println();
-        
+
         boolean isCorrect = false;
-        
-        for ( User u : authorizedUsers)
+
+        for (User u : authorizedUsers)
         {
-            
+
             isCorrect = compareUsers(testUser, u);
-            
+
             if (isCorrect)
+            {
                 break;
-            
+            }
+
         }
-        
+
         System.out.print(isCorrect);
-       
+
         return isCorrect;
-   
+
     }
-    
+
     public static boolean compareUsers(User u1, User u2)
     {
-        
-        if ( u1.username ==  u2.username && Arrays.equals(u1.password, u2.password))
-                return true;
+        System.out.println("Compairing two users:");
+        System.out.println("Compare usernames: " + u1.username + ", "
+                + u2.username);
+        System.out.print("Compare passwords: ");
+
+        for (char c : u1.password)
+        {
+            System.out.print(c);
+        }
+
+        System.out.print(", ");
+
+        for (char c : u2.password)
+        {
+            System.out.print(c);
+        }
+
+        System.out.println();
+
+        if (u1.username.equals(u2.username)
+                && Arrays.equals(u1.password, u2.password))
+            return true;
         else
-                return false;
-        
+            return false;
+
     }
-    
-    
-    // Defines a helper methods to read account data from and write to an binary data file.
-    
+
+    // Defines a helper methods to read account data from and write to an binary
+    // data file.
+
     public static DataInputStream getStream(String name)
     {
-        
+
         DataInputStream in = null;
-        
+
         try
         {
-            
+
             File file = new File(name);
-            in = new DataInputStream(new BufferedInputStream(new FileInputStream(file)));
-            
-        }
-        catch ( FileNotFoundException e)
+            in = new DataInputStream(new BufferedInputStream(
+                    new FileInputStream(file)));
+
+        } catch (FileNotFoundException e)
         {
-            
+
             System.out.println("The file was not found.");
             System.exit(0);
-            
-        }
-        catch (IOException e)
+
+        } catch (IOException e)
         {
-            
+
             System.out.println("I/O Error creating file.");
             System.exit(0);
-            
+
         }
         return in;
-        
+
     }
-    
+
     public static Account readAccount(DataInputStream in)
     {
-        
+
         String name = " ";
         float bal = 0;
         float apr = 0;
-        
+
         try
         {
-            
+
             name = in.readUTF();
             bal = in.readFloat();
             apr = in.readFloat();
-            
-        }
-        catch (EOFException e)
+
+        } catch (EOFException e)
         {
-            
+
             return null;
-            
-        }
-        catch (IOException e)
+
+        } catch (IOException e)
         {
-            
+
             System.out.println("I/O Error");
             System.exit(0);
-            
+
         }
-        
+
         return new Account(name, bal, apr);
-        
+
     }
-    
-    public static User[] readUsers(DataInputStream in)
-    {
-        
-        String name = " ";
-        char[] pass;
-        
-        try
-        {
-            
-            name = in.readUTF();
-            bal = in.readFloat();
-            apr = in.readFloat();
-            
-        }
-        catch (EOFException e)
-        {
-            
-            return null;
-            
-        }
-        catch (IOException e)
-        {
-            
-            System.out.println("I/O Error");
-            System.exit(0);
-            
-        }
-        
-        return new Account(name, bal, apr);
-        
-    }
-    
+
     public static void closeFile(DataInputStream in)
     {
-        
+
         try
         {
-            
+
             in.close();
-            
-        }
-        catch (IOException e)
+
+        } catch (IOException e)
         {
-            
+
             System.out.println("I/O Error closing file.");
             System.out.println();
-            
+
         }
     }
-    
-    
-    // Helper Method to check and see if file exists. 
+
+    // Helper Method to check and see if file exists.
     public static boolean doesFileExist(Path p)
     {
-        
+
         if (!Files.exists(p))
         {
-            
+
             System.out.println("File does not exist.");
-            
+
             return false;
-            
-        }
-        else
+
+        } else
             return true;
-            
-            
+
     }
-    
+
     // Helper method to create a file
     public static void createFile(Path p)
     {
-        
+
         try
         {
-            
+
             Files.createFile(p);
             System.out.println("File created!");
-            
-        }
-        catch(Exception e)
+
+        } catch (Exception e)
         {
-            
+
             System.out.println("Error: " + e.getMessage());
-            
+
         }
     }
-    
-    
-    public static void saveToFile(Object e, String file)
+
+    public static void saveUser(User e, String filename, boolean append)
     {
 
-       try
-       {
-          FileOutputStream fileOut =
-          new FileOutputStream(file);
-          ObjectOutputStream out =
-                             new ObjectOutputStream(fileOut);
-          out.writeObject(e);
-          out.close();
-           fileOut.close();
-       }catch(IOException i)
-       {
-           i.printStackTrace();
-       }
+        File file = new File(filename);
+        ObjectOutputStream out = null;
+
+        try
+        {
+            /*
+             * KeyGenerator kg = KeyGenerator.getInstance("DES"); kg.init(new
+             * SecureRandom()); SecretKey key = kg.generateKey();
+             * SecretKeyFactory skf = SecretKeyFactory.getInstance("DES"); Class
+             * spec = Class.forName("javax.crypto.spec.DESKeySpec"); DESKeySpec
+             * ks = (DESKeySpec) skf.getKeySpec(key, spec); ObjectOutputStream
+             * oos = new ObjectOutputStream(new
+             * FileOutputStream(KEY_FILE.toString()));
+             * oos.writeObject(ks.getKey());
+             * 
+             * Cipher c = Cipher.getInstance("DES/CFB8/NoPadding");
+             * c.init(Cipher.ENCRYPT_MODE, key); CipherOutputStream cos = new
+             * CipherOutputStream(new FileOutputStream(file), c);
+             */
+
+            if (!file.exists() || !append)
+            {
+                out = new ObjectOutputStream(new FileOutputStream(filename));
+            } else
+            {
+                out = new AppendableObjectOutputStream(new FileOutputStream(
+                        filename, append));
+            }
+
+            if (e != null)
+            {
+                System.out.println("Saving User: " + e.toString());
+            } else
+            {
+                System.out.println("Writing Null pointer.");
+            }
+
+            out.writeObject(e);
+            out.flush();
+
+            // oos.writeObject(c.getIV());
+            // oos.close();
+
+        } catch (IOException i)
+        {
+
+            i.printStackTrace();
+
+        } finally
+        {
+            try
+            {
+                if (out != null)
+                {
+                    out.close();
+                }
+
+            } catch (Exception ex)
+            {
+                ex.printStackTrace();
+
+            }
+        }
     }
-    
-    public static User[] readUser(String file)
+
+    public static User[] readUser(String filename)
     {
-       
-       User[] users = null;
-        
-       try
-       {
-          FileInputStream fileIn =
-                           new FileInputStream(file);
-          ObjectInputStream in = new ObjectInputStream(fileIn);
-          
-          boolean eof = false;
-          
-          int i = 0;
-          
-          while (!eof)
-          {
-              
-              User tempUser = (User) in.readObject();
-           
-              if (tempUser == null)
-                  eof = true;
-              else
-                  users[i] = tempUser;
-             
-              i++;
-              
-          }
-          
-          in.close();
-          fileIn.close();
-          
-       }catch(IOException i)
-       {
-          i.printStackTrace();
-          System.exit(0);
-       }catch(ClassNotFoundException c)
-       {
-          System.out.println("User class not found");
-          c.printStackTrace();
-          System.exit(0);
-       }
-       
-    return users;
-      
+
+        File file = new File(filename);
+
+        if (file.exists())
+        {
+
+            ObjectInputStream ois = null;
+
+            try
+            {
+
+                ArrayList<User> userList = new ArrayList<User>();
+                ois = new ObjectInputStream(new FileInputStream(filename));
+                User tempUser = null;
+
+                while ((tempUser = (User) ois.readObject()) != null)
+                {
+                    System.out.println("Read User: " + tempUser.toString());
+                    userList.add(tempUser);
+
+                }
+
+                User[] users = userList.toArray(new User[userList.size()]);
+
+                return users;
+            } catch (EOFException e)
+            {
+            } catch (Exception e)
+            {
+                e.printStackTrace();
+            } finally
+            {
+
+                try
+                {
+                    if (ois != null)
+                    {
+                        ois.close();
+                    }
+
+                } catch (IOException e)
+                {
+
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return null;
+
     }
-    
-    
+
+    private static class AppendableObjectOutputStream extends
+            ObjectOutputStream
+    {
+        public AppendableObjectOutputStream(OutputStream out)
+                throws IOException
+        {
+            super(out);
+        }
+
+        @Override
+        protected void writeStreamHeader() throws IOException
+        {
+        }
+    }
+
     // Defines and returns the standard Header Box for the DB9000 program.
     public static Box createHeader(String title)
     {
-        
+
         Box htextBox = Box.createHorizontalBox();
         JLabel headerTitle = new JLabel(title);
         headerTitle.setFont(new Font("Elephant", Font.PLAIN, 31));
@@ -375,8 +431,7 @@ public class Reference
         headerBox.add(hbBox);
 
         return headerBox;
-        
+
     }
-    
+
 }
-   
